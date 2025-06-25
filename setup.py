1e2ffc0a954b12a5f8d1237ad58e9afa418ef629
Tin Popovic <tinpopovic@Tins-MacBook-Pro.local>
2025-06-25 11:28:31 +0200

    Add a setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rct',
+    version='0.1',
+    description='Compute the RCT (Relevance–Cost–Time) index for evaluating AI systems',
+    author='Tin Popović',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.7',
+)
